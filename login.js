a288fbd2cf66fcf10590d0e0c980724e2da85abf
Tony <gsn94561266@gmail.com>
2023-08-31 17:27:40 +0800

    update

--- login.js
+++ login.js
@@ -22,7 +22,6 @@ const initialize = () => {
   const passwordInput = document.querySelector("#password-input");
   const loginBtn = document.querySelector("#login-btn");
   const errorMessage = document.querySelector("#error-message");
-  let imageRatioInitialized = false;
   let errorMessageTimeout;
 
   const setFormStyle = (imageSrc, imageRatio) => {
@@ -55,11 +54,6 @@ const initialize = () => {
   };
 
   const handleResize = () => {
-    const isSafariMobile =
-      /iPhone|iPad|iPod/i.test(navigator.userAgent) &&
-      /Safari/i.test(navigator.userAgent);
-    if (isSafariMobile && imageRatioInitialized) return;
-
     const screenWidth = window.innerWidth;
     const screenHeight = window.innerHeight;
     const screenRatio = screenWidth / screenHeight;
@@ -80,10 +74,7 @@ const initialize = () => {
         ? Math.min(screenWidth / imageWidth, scrollHeight / imageHeight)
         : Math.max(screenWidth / imageWidth, scrollHeight / imageHeight);
 
-      if (!imageRatioInitialized) {
-        setFormStyle(imageSrc, imageRatio);
-        imageRatioInitialized = true;
-      }
+      setFormStyle(imageSrc, imageRatio);
     };
 
     document.documentElement.style.setProperty(
