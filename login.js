18f095ae4d3fc3ae4d785540b9522419e656b2ca
Tony <gsn94561266@gmail.com>
2023-08-31 11:14:44 +0800

    update

--- login.js
+++ login.js
@@ -140,4 +140,16 @@ const doFirst = () => {
   window.addEventListener("orientationchange", handleResize);
 };
 
+const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+if (isSafari) {
+  document.body.addEventListener(
+    "touchmove",
+    (e) => {
+      e.preventDefault();
+    },
+    { passive: false }
+  );
+}
+
 window.addEventListener("load", doFirst);
