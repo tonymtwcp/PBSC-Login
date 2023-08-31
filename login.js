f0685a19aedc92a8c38d4f7cac1ed2d16996682a
Tony <gsn94561266@gmail.com>
2023-08-31 16:35:35 +0800

    update

--- login.js
+++ login.js
@@ -1,40 +1,47 @@
 const formStyles = {
   "./images/PBSC-169.jpg": {
-    formBoxStyle: { width: 17, height: 21.2 },
-    inputBoxStyle: { height: 80, marginLeft: 60, marginBottom: 21 },
-    inputFieldStyle: { width: 166, height: 28, fontSize: 16 },
+    LoginformStyle: { width: 17, height: 21.2 },
+    inputContainerStyle: { height: 80, marginLeft: 60, marginBottom: 21 },
+    inputFieldStyle: { width: 166, height: 28 },
     loginBtnStyle: { width: 90, height: 45 },
   },
   "./images/PBSC-43.jpg": {
-    formBoxStyle: { width: 13.6, height: 21 },
-    inputBoxStyle: { height: 80, marginLeft: 45, marginBottom: 25 },
+    LoginformStyle: { width: 13.6, height: 21 },
+    inputContainerStyle: { height: 80, marginLeft: 45, marginBottom: 25 },
     inputFieldStyle: { width: 118, height: 28 },
     loginBtnStyle: { width: 68, height: 39 },
   },
 };
 
-const doFirst = () => {
-  const imageElement = document.querySelector("#bg-img");
-  const formBox = document.querySelector("#form-box");
-  const inputBox = document.querySelector("#input-box");
+const initialize = () => {
+  const bgImage = document.querySelector("#bg-img");
+  const Loginform = document.querySelector("#login-form");
+  const inputContainer = document.querySelector("#input-container");
   const inputFields = document.querySelectorAll(".input-field");
-  const accountInput = document.querySelector("#account");
-  const passwordInput = document.querySelector("#password");
+  const accountInput = document.querySelector("#account-input");
+  const passwordInput = document.querySelector("#password-input");
   const loginBtn = document.querySelector("#login-btn");
   const errorMessage = document.querySelector("#error-message");
+  let errorMessageTimeout;
 
   const setFormStyle = (imageSrc, imageRatio) => {
     const formStyle = formStyles[imageSrc];
     if (formStyle) {
-      const { formBoxStyle, inputBoxStyle, inputFieldStyle, loginBtnStyle } =
-        formStyle;
+      const {
+        LoginformStyle,
+        inputContainerStyle,
+        inputFieldStyle,
+        loginBtnStyle,
+      } = formStyle;
 
-      formBox.style.transform = `scale(${imageRatio})`;
-      formBox.style.width = `${formBoxStyle.width}rem`;
-      formBox.style.height = `${formBoxStyle.height}rem`;
-      inputBox.style.height = `${inputBoxStyle.height}px`;
-      inputBox.style.marginLeft = `${inputBoxStyle.marginLeft}px`;
-      inputBox.style.marginBottom = `${inputBoxStyle.marginBottom}px`;
+      Loginform.style.transform = `scale(${imageRatio})`;
+
+      Loginform.style.width = `${LoginformStyle.width}rem`;
+      Loginform.style.height = `${LoginformStyle.height}rem`;
+
+      inputContainer.style.height = `${inputContainerStyle.height}px`;
+      inputContainer.style.marginLeft = `${inputContainerStyle.marginLeft}px`;
+      inputContainer.style.marginBottom = `${inputContainerStyle.marginBottom}px`;
 
       inputFields.forEach((inputField) => {
         inputField.style.width = `${inputFieldStyle.width}px`;
@@ -55,12 +62,12 @@ const doFirst = () => {
       screenRatio > 1.9 || screenRatio < 1
         ? "./images/PBSC-169.jpg"
         : "./images/PBSC-43.jpg";
-    imageElement.src = imageSrc;
+    bgImage.src = imageSrc;
 
-    imageElement.onload = () => {
+    bgImage.onload = () => {
       const scrollHeight = document.documentElement.scrollHeight;
-      const imageWidth = imageElement.naturalWidth;
-      const imageHeight = imageElement.naturalHeight;
+      const imageWidth = bgImage.naturalWidth;
+      const imageHeight = bgImage.naturalHeight;
 
       const shouldUseMin = scrollHeight > screenHeight;
       const imageRatio = shouldUseMin
@@ -77,8 +84,6 @@ const doFirst = () => {
   };
 
   const showError = (message) => {
-    let errorMessageTimeout;
-
     if (!errorMessage) {
       console.error("Error message element not found.");
       return;
@@ -105,25 +110,23 @@ const doFirst = () => {
       return;
     }
 
-    const apiUrl = "LOGIN_API_URL";
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ account, password }),
-    };
-
     try {
-      const response = await fetch(apiUrl, requestOptions);
-      const data = await response.json();
+      const res = await fetch("LOGIN_API_URL", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({ account, password }),
+      });
+      const data = await res.json();
 
       if (data.error) {
         showError(data.error);
         passwordInput.value = "";
       }
-    } catch (error) {
-      console.error(error);
+    } catch (e) {
+      console.error(e);
       showError("An error occurred, please try again later.");
       passwordInput.value = "";
     }
@@ -131,7 +134,7 @@ const doFirst = () => {
 
   loginBtn.addEventListener("click", handleLogin);
 
-  formBox.addEventListener("submit", (e) => {
+  Loginform.addEventListener("submit", (e) => {
     e.preventDefault();
   });
 
@@ -140,4 +143,4 @@ const doFirst = () => {
   window.addEventListener("orientationchange", handleResize);
 };
 
-window.addEventListener("load", doFirst);
+window.addEventListener("load", initialize);
